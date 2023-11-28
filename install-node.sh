7d88fcc0f693a9b30c8f8d9f7753d65bf2ebc66c
Jose Daniel Baena <jbaena57@summa-sci.com>
2023-11-28 15:52:34 -0500

    add .sh and action.yml

---
+++ install-node.sh
@@ -0,0 +1,10 @@
+set -e
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install $1
+nvm alias default $1
+nvm use default
